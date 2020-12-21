a8026b25d619be2429663ae8dd6d256cc9598cd2
AGajadharL2021 <75976673+AGajadharL2021@users.noreply.github.com>
2020-12-21 11:00:54 +0100

    Update JavaApplication.java

--- JavaApplication.java
+++ JavaApplication.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editorr.
- */
+
 package javaapplication;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * El programador B creará los metodos necesarios para: (seguir instrucciones)
+/*
  *kkkkk
- * @author  Pon tu nombre Programador B
+ * @author  Alejandro
  */
 public class Entrada {
 
@@ -26,34 +21,21 @@ public class Entrada {
     public static int entero() {
         int aux = 0;
         
-        /* Definir un scanner para poder leer por teclado*/
-        
-        //Scanner teclado = .......
         
         Scanner teclado = new Scanner (System.in);
        
         try {
-            /*Leer por teclado y asigar a aux*/
-            //aux=
             aux = teclado.nextInt();
-            
-            /*Si el usuario introduce un numero mayor menor de 100 lanzar
-              un throw de tipo IllegalArgumentException */
             if(aux<100){
                 throw new IllegalArgumentException("El numero es menor a 100");
             }
-           
-            //Aqui va el throw
         } catch (IllegalArgumentException e){
             System.out.println(e);
         }
         return aux;
     }
     
-    /**
-     * Crear a continuación un metodo parecida a la anterior
-     * que pida una cadena de caracteres. (sin Throw)
-     */
+  
     
    /**
      * Metodo que pide un numero entero al usuario
